46642969b84e141c4a06a4d9ece423335ebbd0a2
Anthony Green <green@moxielogic.com>
2021-08-01 09:43:33 -0400

    Change license. Update text.

--- make-like.lisp
+++ make-like.lisp
@@ -1,3 +1,28 @@
+;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: MAKE-LIKE; Base: 10 -*-"
+
+;;; -----------------------------------------------------------------------
+;;; make-like
+;;;
+;;; Permission is hereby granted, free of charge, to any person obtaining
+;;; a copy of this software and associated documentation files (the
+;;; ``Software''), to deal in the Software without restriction, including
+;;; without limitation the rights to use, copy, modify, merge, publish,
+;;; distribute, sublicense, and/or sell copies of the Software, and to
+;;; permit persons to whom the Software is furnished to do so, subject to
+;;; the following conditions:
+;;;
+;;; The above copyright notice and this permission notice shall be included
+;;; in all copies or substantial portions of the Software.
+;;;
+;;; THE SOFTWARE IS PROVIDED ``AS IS'', WITHOUT WARRANTY OF ANY KIND,
+;;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;;; NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+;;; WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;;; DEALINGS IN THE SOFTWARE.
+;;; -----------------------------------------------------------------------
 
 (in-package #:make-like)
 
@@ -16,7 +41,7 @@
 (defun usage ()
   (opts:describe
    :prefix "make-like - copyright (C) 2021 Anthony Green <green@moxielogic.com>"
-   :suffix "Distributed under the terms of MIT License"
+   :suffix "Distributed under the terms of MIT License.  See the source code for details."
    :usage-of "make-like"
    :args     "likefile"))
 
